6e9bfd8300a409f89e594657a1a43d0232f366c2
Gaya <gaya.kessler@gmail.com>
2013-09-20 21:31:55 +0200

    Boilerplate files version 1

--- assets/compass/config.rb
+++ assets/compass/config.rb
@@ -4,11 +4,10 @@
 relative_assets = true      #because we're not working from the root
 css_dir = "../css"          #where the CSS will saved
 sass_dir = "sass"           #where our .scss files are
-images_dir = "../img"       #the folder with your images
-fonts_dir = "../fonts"      #the folder with your fonts
+images_dir = "../images"    #the folder with your images
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :nested # After dev :compressed
+output_style = :expanded # After dev :compressed
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = true
