336a66ac7ad96a9fdfa460be4ca721ae03f2787f
Gaya <gaya.kessler@gmail.com>
2013-09-20 19:55:03 +0200

    Initial config file

---
+++ assets/compass/config.rb
@@ -0,0 +1,17 @@
+# Require any additional compass plugins here.
+
+#Folder settings
+relative_assets = true      #because we're not working from the root
+css_dir = "../css"          #where the CSS will saved
+sass_dir = "sass"           #where our .scss files are
+images_dir = "../img"       #the folder with your images
+fonts_dir = "../fonts"      #the folder with your fonts
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :nested # After dev :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+
+# Obviously
+preferred_syntax = :scss
